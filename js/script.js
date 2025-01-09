18fb21b693ddf5533368899112bed59b6a0bd165
ko-nagai-0801 <ko.nagai.0801@gmail.com>
2025-01-09 11:10:03 +0900

    ２回目のコミット

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Hamburger Menu Toggle
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  hamburger.classList.toggle("toggle");
+});
