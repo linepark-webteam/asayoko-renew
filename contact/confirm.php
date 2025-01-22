5ccfd18b8de9ca1ec3c6efa66a1aede62a32bd0e
ko-nagai-0801 <ko.nagai.0801@gmail.com>
2025-01-22 09:12:16 +0900

    JSファイル名修正

--- contact/confirm.php
+++ contact/confirm.php
@@ -157,7 +157,7 @@ $message = $_SESSION['message'] ?? '';
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
         crossorigin="anonymous"></script>
-    <script src="../js/script.js"></script>
+    <script src="../js/common.js"></script>
 </body>
 
 </html>
